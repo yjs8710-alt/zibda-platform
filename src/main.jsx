f929c25bd07d80cfe9d97b0ab6812d990b845062
yjs8710 <your@email.com>
2026-06-29 15:38:45 +0900

    Simplify to static HTML site

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-document.getElementById('root').innerHTML = `
-  <div style="padding: 40px; text-align: center; font-family: sans-serif;">
-    <h1>🚀 Zibda Platform</h1>
-    <p>부동산 공실 관리 플랫폼</p>
-    <p>Vercel 배포 완료!</p>
-  </div>
-`;
